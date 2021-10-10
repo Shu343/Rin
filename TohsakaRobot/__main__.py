ee2d49dc389d1f1a477f4b6ecf47cfdf61b17b39
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-10-10 17:44:07 +0530

    Update __main__.py

--- TohsakaRobot/__main__.py
+++ TohsakaRobot/__main__.py
@@ -41,7 +41,7 @@ PM_START_TEXT = """
 Hi {}, my name is {}!
 I'm a group management bot with a few fun extras ;)
 
-The support chat is at @tohsakas
+The support chat is at @Aqua_Support
 
 My repository: [Rin](https://github.com/EagleUnion/Rin)
 
@@ -66,7 +66,7 @@ And the following:
     "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n",
 )
 
-RIN_IMG = "https://telegra.ph/file/2dc1c31660b79b7e3ce90.jpg"
+RIN_IMG = "https://telegra.ph/file/cb9ee39a95f0653f99e67.jpg"
 
 DONATE_STRING = """I'm free for everyone uwu""".format(dispatcher.bot.first_name)
 
