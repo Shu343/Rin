5c54c5044162ca7742425ce4087e348e5c83a4ee
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-10-10 14:44:15 +0530

    Update sample_config.py

--- TohsakaRobot/sample_config.py
+++ TohsakaRobot/sample_config.py
@@ -14,9 +14,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "3395247"
+    OWNER_ID = "c0c4adf40b0b3f76b07ffe613ce6ca8d"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "Its_shu3"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = "sqldbtype://username:pw@hostname:port/db_name"  # needed for any database modules
@@ -42,6 +42,7 @@ class Config(object):
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
+    TOKEN = '2060272671:AAEOtO1M1AwhPFdPhwowJxqkKmOJBliixbI'
     STRICT_GBAN = False
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
     BAN_STICKER = "CAADAgADOwADPPEcAXkko5EB3YGYAg"  # banhammer marie sticker
