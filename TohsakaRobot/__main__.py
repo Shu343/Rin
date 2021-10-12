498fec7504c8c54b3d2106045e98a8b90dec5620
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-10-12 20:11:32 +0530

    Update __main__.py

--- TohsakaRobot/__main__.py
+++ TohsakaRobot/__main__.py
@@ -49,22 +49,8 @@ You can find the list of available commands with /help
 """
 
 HELP_STRINGS = """
-Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras!
-
-*Main* commands available:
- - /start: start the bot
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
-{}
-And the following:
-""".format(
-    dispatcher.bot.first_name,
-    "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n",
-)
+<B>All Helpable commands are here</B>
+"""
 
 RIN_IMG = "https://telegra.ph/file/2dc1c31660b79b7e3ce90.jpg"
 
