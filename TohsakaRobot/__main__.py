272502ed85e649698acecd65dffff3e376602a23
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-10-13 07:03:02 +0530

    Update __main__.py

--- TohsakaRobot/__main__.py
+++ TohsakaRobot/__main__.py
@@ -49,7 +49,7 @@ You can find the list of available commands with /help
 """
 
 HELP_STRINGS = """
-<b>All Helpable commands are here</b>
+All Helpable commands are here:
 """.format(
     parse_mode=ParseMode.HTML,
 )
