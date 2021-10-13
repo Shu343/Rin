0f608682bea8a8de3cfee0af04a0c84bdb922739
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-10-13 06:55:12 +0530

    Update __main__.py

--- TohsakaRobot/__main__.py
+++ TohsakaRobot/__main__.py
@@ -49,8 +49,8 @@ You can find the list of available commands with /help
 """
 
 HELP_STRINGS = """
-<b>All Helpable commands are here</b>
-""""=parse_mode=ParseMode.HTML
+<b>All Helpable commands are here</b>(parse_mode=ParseMode.HTML)
+"""
 
 RIN_IMG = "https://telegra.ph/file/2dc1c31660b79b7e3ce90.jpg"
 
