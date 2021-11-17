c8ca53213226d876138776496ed742d5f218c369
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-11-17 07:19:09 +0530

    code formatting

--- src/modules/blacklist.py
+++ src/modules/blacklist.py
@@ -6,12 +6,12 @@ from telegram import Message, Chat, Update, Bot, ParseMode
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
 
-import TohsakaRobot.modules.sql.blacklist_sql as sql
-from TohsakaRobot import dispatcher, LOGGER
-from TohsakaRobot.modules.disable import DisableAbleCommandHandler
-from TohsakaRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from TohsakaRobot.modules.helper_funcs.extraction import extract_text
-from TohsakaRobot.modules.helper_funcs.misc import split_message
+import src.modules.sql.blacklist_sql as sql
+from src import dispatcher, LOGGER
+from src.modules.disable import DisableAbleCommandHandler
+from src.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from src.modules.helper_funcs.extraction import extract_text
+from src.modules.helper_funcs.misc import split_message
 
 BLACKLIST_GROUP = 11
 
