6d6850fb374a39c9105e449a556f4407d3c7c32f
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-11-18 17:30:30 +0530

    code formatting

--- src/modules/helper_funcs/extraction.py
+++ src/modules/helper_funcs/extraction.py
@@ -3,8 +3,8 @@ from typing import List, Optional
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
-from TohsakaRobot import LOGGER
-from TohsakaRobot.modules.users import get_user_id
+from src import LOGGER
+from src.modules.users import get_user_id
 
 
 def id_from_reply(message):
