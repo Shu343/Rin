73d7ada400200a3e02ed539fab393b72e163dffd
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-11-18 19:51:12 +0530

    code formatting

--- src/modules/sql/locks_sql.py
+++ src/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from TohsakaRobot.modules.sql import SESSION, BASE
+from src.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
