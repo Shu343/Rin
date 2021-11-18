bf795316f01209604d2f6fc7850dffdb91ffabe6
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-11-18 19:41:50 +0530

    code formatting

--- src/modules/sql/antiflood_sql.py
+++ src/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, Integer, String
 
-from TohsakaRobot.modules.sql import BASE, SESSION
+from src.modules.sql import BASE, SESSION
 
 DEF_COUNT = 0
 DEF_LIMIT = 0
