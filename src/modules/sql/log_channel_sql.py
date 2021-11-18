2a8045871d2da6bdac731351e9df63a9e6340c54
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-11-18 19:52:33 +0530

    code formatting

--- src/modules/sql/log_channel_sql.py
+++ src/modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, func, distinct
 
-from TohsakaRobot.modules.sql import BASE, SESSION
+from src.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
