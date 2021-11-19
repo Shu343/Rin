6d880967d1d915b4a918f7bb2be3fdb3c5122b38
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-11-19 22:12:43 +0530

    Small help menu

--- src/modules/helper_funcs/misc.py
+++ src/modules/helper_funcs/misc.py
@@ -74,6 +74,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     elif calc == 2:
         pairs.append((modules[-1],))
 
+    max_num_pages = ceil(len(pairs) / 10)
+    modulo_page = page_n % max_num_pages
+
     else:
         pairs += [[EqInlineKeyboardButton("GO to Home", callback_data="start_back")
                                           
