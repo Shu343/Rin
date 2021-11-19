192c30f91883ec62625b6cf1f3e5ca0975b83926
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-11-19 17:32:21 +0530

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -254,7 +254,7 @@ def help_button(bot: Bot, update: Update):
 
         # ensure no spinny white circle
         bot.answer_callback_query(query.id)
-        query.message.edit()
+        query.message.edit_text()
     except BadRequest as excp:
         if excp.message == "Message is not modified":
             pass
