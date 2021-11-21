3d61035d075e46e0b1605ad268baa40c25531e4d
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-11-21 21:42:45 +0530

    Update chatbot.py

--- src/modules/chatbot.py
+++ src/modules/chatbot.py
@@ -94,7 +94,6 @@ def kuki(update: Update, bot: Bot) -> str:
            parse_mode=ParseMode.HTML,
        )
 
-
 def kuki_message(context: CallbackContext, message)
     reply_message = message.reply_to_message
     if message.text.lower() == "kuki":
