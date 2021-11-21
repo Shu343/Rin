e03e7cae9da5d4a94fdd73d9745c36275a9d3d4d
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-11-21 07:22:45 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -139,9 +139,6 @@ else:
 
 updater = tg.Updater(TOKEN, workers=WORKERS)
 
-bot = Client("Ryougi", bot_token=TOKEN, api_id=6,
-             api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e")
-dispatcher = updater.dispatcher
 
 SUDO_USERS = list(SUDO_USERS)
 WHITELIST_USERS = list(WHITELIST_USERS)
