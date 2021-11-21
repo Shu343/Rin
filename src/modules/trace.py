168fc343fd33cd02244368333c42cd413ae093b9
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-11-21 07:14:40 +0530

    Update trace.py

--- src/modules/trace.py
+++ src/modules/trace.py
@@ -1,15 +1,17 @@
 import tracemoepy
 from pyrogram import filters
-from telegram.ext.dispatcher import run_async
-from telegram.ext import CommandHandler
-from src import dispatcher
+from src import TOKEN
 import os
+
+bot = Client("Ryougi", bot_token=TOKEN, api_id=6,
+             api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+
 
 tracemoe = tracemoepy.tracemoe.TraceMoe()
 
-@run_async
-def whatanime(update, bot):
-  reply = update.effective_message.reply_message()
+@bot.on_message(filters.command('whatanime'))
+def whatanime(_,message):
+  reply = message.reply_to_message
   if reply and reply.media:
     path = reply.download()
     info = tracemoe.search(path, upload_file=True)
@@ -17,15 +19,4 @@ def whatanime(update, bot):
     info.result[0].save(f"{reply.from_user.id}.mp4", mute = False)
     reply.reply_document(f"{reply.from_user.id}.mp4" , caption=data)
     os.remove(f"{reply.from_user.id}.mp4")
-
-__help__ = """
-Find which anime a gif, video or image is from using trace.moe API.
-
-/whatanime (on reply to media) - initiate search  
-"""
-__mod_name__ = "Trace.moe"
-
-WHATANIME_HANDLER = CommandHandler("whatanime", whatanime)
-
-dispatcher.add_handler(WHATANIME_HANDLER)
-  
+    
