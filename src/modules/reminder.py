0b7b44c3bf911107646033fc73a8552019d397af
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2021-12-07 11:19:38 +0530

    Update reminder.py

--- src/modules/reminder.py
+++ src/modules/reminder.py
@@ -1,15 +1,15 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, context
+from telegram.ext import Updater, CommandHandler
 from src import dispatcher
 
 
-def alarm(context) -> None:
+def alarm(context: CallbackContext) -> None:
     """Send the alarm message."""
     job = context.job
     context.bot.send_message(job.context, text='Beep!')
 
 
-def remove_job_if_exists(name: str, context) -> bool:
+def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
     """Remove job with given name. Returns whether job was removed."""
     current_jobs = context.job_queue.get_jobs_by_name(name)
     if not current_jobs:
@@ -19,7 +19,7 @@ def remove_job_if_exists(name: str, context) -> bool:
     return True
 
 
-def set_timer(update, context) -> None:
+def set_timer(update: Update, context: CallbackContext) -> None:
     """Add a job to the queue."""
     chat_id = update.message.chat_id
     try:
@@ -41,7 +41,7 @@ def set_timer(update, context) -> None:
         update.message.reply_text('Usage: /set <seconds>')
 
 
-def unset(update, context) -> None:
+def unset(update: Update context: CallbackContext) -> None:
     """Remove the job if the user changed their mind."""
     chat_id = update.message.chat_id
     job_removed = remove_job_if_exists(str(chat_id), context)
