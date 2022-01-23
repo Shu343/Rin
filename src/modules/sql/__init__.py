eb6a8493f67177bd1122caddceb018c65ad11662
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2022-01-23 11:49:11 +0530

    Update __init__.py

--- src/modules/sql/__init__.py
+++ src/modules/sql/__init__.py
@@ -1,16 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from src import DB_URL
+class SomeTable(Base):
+    """
+    Class, that represents SomeTable
+    """
+    __tablename__ = "my_table_in_db"
 
-from src import DB_URI
+    __table__ = Table(__tablename__, Base.metadata,
+        autoload=True,
+        autoload_with=create_engine(DB_URL))
 
-
-def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
-
-
-BASE = declarative_base()
-SESSION = start()
